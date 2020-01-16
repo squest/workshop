af2ba4e6f0d0ef1d2c3eb1b5c5802ff5851a8f44
Sabda PS <sabdaps@gmail.com>
2020-01-17 01:43:53 +0700

    editing the resync namespace

--- homey/app/src/app/resync.clj
+++ homey/app/src/app/resync.clj
@@ -9,8 +9,9 @@
 
 (defn open-file
   [which-season which-one]
-  (->> (str base-folder "Season" which-season "/" which-one ".srt")
-       slurp))
+  (let [res (->> (str base-folder "Season" which-season "/" which-one ".srt")
+                 slurp)]
+    res))
 
 (defn separate
   [txt]
@@ -24,15 +25,15 @@
 
 (defn rules
   [mins secs amount]
-  (let [totals (+ secs (* 60 mins))]
-    (cond
-      (< totals amount) 0
-      (<= mins 13) (+ secs (* 60 mins))
-      (< 13 mins 23) (- (+ (* 60 mins) secs) 3)
-      (<= 23 mins 28) (- (+ (* 60 mins) secs) 4)
-      (< 28 mins 33) (- (+ (* 60 mins) secs) 5)
-      (<= 33 mins 38) (- (+ (* 60 mins) secs) 6)
-      (< 38 mins 45) (- (+ (* 60 mins) secs) 7))))
+  (let [totals (+ secs (* 60 mins))
+        res (cond
+              (< totals amount) 0
+              (<= mins 15) (- (+ secs (* 60 mins)) 1)
+              (< 15 mins 25) (- (+ (* 60 mins) secs) 2)
+              (<= 25 mins 30) (- (+ (* 60 mins) secs) 3)
+              (<= 30 mins 35) (- (+ (* 60 mins) secs) 4)
+              (< 35 mins 450) (- (+ (* 60 mins) secs) 5))]
+    res))
 
 (defn convert
   [time-string amount]
@@ -41,25 +42,28 @@
         [secs sec2] (cs/split sec12 #",")
         int-mins (bigint mins)
         int-secs (bigint secs)
-        allsecs (rules int-mins int-secs amount)]
-    (str "00:"
-         (dform (quot allsecs 60))
-         ":"
-         (dform (rem allsecs 60))
-         ","
-         sec2)))
+        allsecs (rules int-mins int-secs amount)
+        res (str "00:"
+                 (dform (quot allsecs 60))
+                 ":"
+                 (dform (rem allsecs 60))
+                 ","
+                 sec2)]
+    res))
 
 (defn convert-both
   [time-string amount]
   (let [[a b] (->> (separate time-string)
-                   (mapv cs/trim))]
-    (str (convert a amount) " --> " (convert b amount))))
+                   (mapv cs/trim))
+        res (str (convert a amount) " --> " (convert b amount))]
+    res))
 
 (defn work
   [line amount]
-  (update-in line
-             [1]
-             #(convert-both % amount)))
+  (let [res (update-in line
+                       [1]
+                       #(convert-both % amount))]
+    res))
 
 (defn convert-all
   [which-season which-one amount]
@@ -68,17 +72,20 @@
                  (partition-by #(= "" %))
                  (remove #(= [""] %))
                  (mapv vec)
-                 (mapv #(work % amount)))]
-    (->> all
-         (mapv #(interpose "\r\n" %))
-         (mapv #(conj (vec %) "\r\n" "\r\n"))
-         (apply concat)
-         (apply str))))
+                 (mapv #(work % amount)))
+        res (->> all
+                 (mapv #(interpose "\r\n" %))
+                 (mapv #(conj (vec %) "\r\n" "\r\n"))
+                 (apply concat)
+                 (apply str))]
+    res))
 
 (defn do-all-files
   [which-season which-files amount]
   (doseq [dfile which-files]
     (println dfile)
     (spit (str base-folder "Season " which-season "/" dfile ".srt")
-          (convert-all which-season dfile amount))))
+          (try (convert-all which-season dfile amount)
+               (catch Exception e
+                 (println "Error " (.getMessage e)))))))
 
